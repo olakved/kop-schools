b7dad57f804b44c9f034da22202802ce80deeb8d
Okutubo Lekan Michael <86299143+olakved@users.noreply.github.com>
2023-01-02 15:01:57 +0100

    home video integration

--- src/Pages/Home/mockData.js
+++ src/Pages/Home/mockData.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { FaGraduationCap, FaBrain, FaCloudsmith } from "react-icons/fa";
 import schoolImg from "../../Assets/Logo/schoolLogo.PNG";
+import bannerVideo from '../../Assets/Home/homeBannerVid.mp4'
+
+
 
 export const HomeData = {
   homeBannerData: {
     title: "Let's be part of your story",
-    video:
-      "https://vod-progressive.akamaized.net/exp=1671845024~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F366%2F18%2F451833843%2F1987949505.mp4~hmac=78953fe0057db7bac201bd363d6b17215f6444f29489c87bda18b73e466beea1/vimeo-prod-skyfire-std-us/01/366/18/451833843/1987949505.mp4?download=1&filename=production+ID%3A5199636.mp4",
+    video: bannerVideo,
     buttonText: "Enroll Now",
   },
 
